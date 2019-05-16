1107550a4a6d1c7098eaedea39df0bd421de04ac
Anthony Rurup <tcrurup@gmail.com>
2019-05-16 23:40:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,6 +14,7 @@ class Student
       VALUES (?, ?)
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")
   end
   
   def self.create_table
