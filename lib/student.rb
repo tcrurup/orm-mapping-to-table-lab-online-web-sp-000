fd1ee07ecb661fc9d374a66d86a6ba6b4f5fe7ce
Anthony Rurup <tcrurup@gmail.com>
2019-05-16 23:35:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,9 +3,9 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
   
-  def initialize
-    @name = ""
-    @grade = ""
+  def initialize(name, grade)
+    @name = name
+    @grade = grade
   end
   
   def self.create_table
